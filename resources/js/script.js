ffafc0b783def5806d26a7d7f7fa7e566341cdda
Guilherme Scaldelai <gui.mochila@gmail.com>
2016-11-06 17:50:24 +0000

    Animation added via JS and CSS. Animate.css library added.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     
+    /* Sticky navigation - Waypoints plugin */
     $('.js-section-features').waypoint(function(direction) {
         if (direction === "down") {
             $('nav').addClass('sticky');
@@ -9,13 +10,58 @@ $(document).ready(function() {
     }, {
         offset: '60px;'
     });
+    
+    /* Scroll on buttons */
+    $('.js-scroll-to-plan').click(function () {
+       $('html, body').animate({scrollTop: $('.js-section-plans').offset().top}, 1000); 
+    });
+    
+    $('.js-scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js-section-features').offset().top}, 1000); 
+    });
 
-    /*
-        var waypoint = new Waypoint({
-          element: document.getElementById('basic-waypoint'),
-          handler: function() {
-            notify('Basic waypoint triggered')
+    /* Navegation scroll */
+    
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
           }
-        })
-    */
+        }
+      });
+    });
+
+    
+    /* Animation on Scroll */
+    
+    $('.js-wp-1').waypoint(function(direction) {
+       $('.js-wp-1').addClass('animated fadeIn'); 
+    }, {
+        offset: '40%'
+    });
+    
+    $('.js-wp-2').waypoint(function(direction) {
+       $('.js-wp-2').addClass('animated fadeInUp'); 
+    }, {
+        offset: '40%'
+    });
+
+    $('.js-wp-3').waypoint(function(direction) {
+       $('.js-wp-3').addClass('animated fadeIn'); 
+    }, {
+        offset: '40%'
+    });
+
+    $('.js-wp-4').waypoint(function(direction) {
+       $('.js-wp-4').addClass('animated pulse'); 
+    }, {
+        offset: '40%'
+    });
+
 });
