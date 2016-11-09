94128eb42c354ecc180f861c2063d9fb549dd6c3
Guilherme Scaldelai <gui.mochila@gmail.com>
2016-11-09 22:16:35 +0000

    Gulp-plumber added to handle errors

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     concat = require('gulp-concat'),
     cleanCSS = require('gulp-clean-css'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber');
 
 // Styles task
 // * Minify CSS files
@@ -11,6 +12,10 @@ var gulp = require('gulp'),
 // Auto-prefix CSS
 gulp.task('styles', function () {
     return gulp.src(['src/css/style.css', 'src/css/**/*.css'])
+        .pipe(plumber(function (err) {
+            console.log('Styles task error: \n' + err);
+            this.emit('end');
+        }))
         .pipe(autoprefixer())
         .pipe(concat('styles.css'))
         .pipe(cleanCSS())
@@ -24,6 +29,10 @@ gulp.task('styles', function () {
 // * Compile script like Babel - Convert ES6 files to ES5.
 gulp.task('scripts', function () {
     return gulp.src('src/js/*.js')
+        .pipe(plumber(function (err) {
+            console.log('Scripts task error: \n' + err);
+            this.emit('end');
+        }))
         .pipe(concat('main.js'))
         .pipe(uglify())
         .pipe(gulp.dest('public/resources/js'))
